625f2c1fb3ad567d07cbbf96a09c8649dc05e0fa
Aleksandra Ujvari <ujvari65@gmail.com>
2023-02-08 19:04:46 +0100

    Create table for houses

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users
+  resources :houses
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
