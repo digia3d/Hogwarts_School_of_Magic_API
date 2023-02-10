bd466e7fa2c5a9987151d96cdd5d8cb0f2c05a5a
Aleksandra Ujvari <ujvari65@gmail.com>
2023-02-10 19:46:17 +0100

    Add controllers for houses

---
+++ app/controllers/houses_controller.rb
@@ -0,0 +1,43 @@
+class HousesController < ApplicationController
+  before_action :set_house, only: [:show, :update, :destroy]
+
+  def index
+    @houses = House.all
+    render json: @houses
+  end
+
+  def show
+    render json: @house
+  end
+
+  def create
+    @house = House.new(house_params)
+    if @house.save
+      render json: @house, status: :created, location: @house
+    else
+      render json: @house.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @house.update(house_params)
+      render json: @house
+    else
+      render json: @house.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @house.destroy
+  end
+
+  private
+
+  def set_house
+    @house = House.find(params[:id])
+  end
+
+  def house_params
+    params.require(:house).permit(:name, :image)
+  end
+end
