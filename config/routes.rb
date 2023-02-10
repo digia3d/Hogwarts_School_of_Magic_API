b4ce926582f991b4cbfd49f99911c71947a0f899
Aleksandra Ujvari <ujvari65@gmail.com>
2023-02-10 19:40:57 +0100

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :characters, only: [:index, :show, :create, :update, :destroy] do
-    resources :wands, only: [:index, :show, :create, :update, :destroy]
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  resources :houses do
+    resources :characters, only: [:index, :show, :create, :update, :destroy] do
+      resources :wands, only: [:index, :show, :create, :update, :destroy]
+    end
   end
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
