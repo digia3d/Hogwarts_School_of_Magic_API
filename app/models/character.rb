00a0483a4e5eef8646d10e81b7cf8a5f9d4b3c11
Aleksandra Ujvari <ujvari65@gmail.com>
2023-02-10 19:35:24 +0100

    Add relationship beatven house, characters and wand

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,4 @@
 class Character < ApplicationRecord
+  belongs_to :house
   has_one :wand
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :users
-  resources :characters, only: [:index, :show, :create, :update, :destroy]
+  resources :characters, only: [:index, :show, :create, :update, :destroy] do
+    resources :wands, only: [:index, :show, :create, :update, :destroy]
+  end
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
