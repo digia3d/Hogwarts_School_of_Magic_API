cbe99640707642c580a350129118d01b9d42403a
Aleksandra Ujvari <ujvari65@gmail.com>
2023-02-18 13:40:13 +0100

    Add data, house and character, to DB

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,6 @@ Rails.application.routes.draw do
     end
   end
   
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Adding a new route for the characters endpoint
+  get '/characters', to: 'characters#index'
 end
