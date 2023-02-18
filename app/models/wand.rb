df382e03ecec91a003106a7ecbd87e0d848527c5
Aleksandra Ujvari <ujvari65@gmail.com>
2023-02-18 12:46:00 +0100

    Add attribute definitions for model Wand, and validations

--- app/models/wand.rb
+++ app/models/wand.rb
@@ -1,3 +1,13 @@
 class Wand < ApplicationRecord
   belongs_to :character
+
+    # attribute definitions
+    attribute :wood, :string
+    attribute :core, :string
+    attribute :length, :integer
+
+    # validations
+    validates :wood, presence: true
+    validates :core, presence: true
+    validates :length, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
