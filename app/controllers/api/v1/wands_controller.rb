091e61440601c85ec031c732f04a43bd472a0ca2
Aleksandra Ujvari <ujvari65@gmail.com>
2023-03-02 20:31:01 +0100

    Create controllers for wands

--- app/controllers/api/v1/wands_controller.rb
+++ app/controllers/api/v1/wands_controller.rb
@@ -1,2 +1,18 @@
 class Api::V1::WandsController < ApplicationController
+  before_action :set_wand, only: [:show]
+
+  def index
+    @wands = Wand.all
+    render json: @wands.as_json(only: [:id, :wood, :core, :length])
+  end
+
+  def show
+    render json: @wand.as_json(only: [:id, :wood, :core, :length])
+  end
+
+  private
+
+  def set_wand
+    @wand = Wand.find(params[:id])
+  end
 end
