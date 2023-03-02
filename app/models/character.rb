240c7701dae27479de7c1a5c5b03572ece6f3c18
Aleksandra Ujvari <ujvari65@gmail.com>
2023-03-02 13:03:30 +0100

    Add attributes in models

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,20 @@
 class Character < ApplicationRecord
   belongs_to :house
   has_one :wand
+
+  # attributes
+
+  attribute :name, :string
+  attribute :species, :string
+  attribute :gender, :string
+  attribute :date_of_birth, :date
+  attribute :ancestry, :string
+  attribute :eye_colour, :string
+  attribute :hair_colour, :string
+  attribute :patronus, :string
+  attribute :hogwarts_student, :boolean
+  attribute :hogwarts_staff, :boolean
+  attribute :actor, :string
+  attribute :alive, :boolean
+  attribute :image, :string
 end
--- app/models/house.rb
+++ app/models/house.rb
@@ -1,2 +1,11 @@
 class House < ApplicationRecord
+  has_many :characters
+
+  # attributes
+
+  attribute :name, :string
+
+  # validations
+
+  validates :name, presence: true
 end
